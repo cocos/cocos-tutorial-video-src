3aa6ce3d0db1d423a0b6b104c98f75ac26efe85c
JoneLau <350791117@qq.com>
2021-10-08 15:06:58 +0800

    video shader03

--- shader/shader2-4/shader.js
+++ shader/shader2-4/shader.js
@@ -51,19 +51,59 @@ function main(){
 
     const vertexSource = `
     attribute vec2 a_position;
+    attribute vec4 a_color;
+
+    varying vec4 v_color;
 
     void main(){
+        v_color = a_color;
         gl_Position = vec4(a_position, 0.0, 1.0);
     }
     `;
     // 根据着色器文本内容，创建 WebGL 上可以使用的着色器对象
     const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
 
+    // const positions = [
+    //     0, 0,
+    //     0.7, 0,
+    //     0, 0.5,
+    // ];
+
     const positions = [
         0, 0,
         0.7, 0,
         0, 0.5,
+        0.7, 0.5
     ];
+
+    const colors = [
+        255, 0, 0, 255,
+        0, 255, 0, 255,
+        0, 0, 255, 255,
+        255, 127, 0, 255
+    ];
+
+    gl.enable(gl.CULL_FACE);
+
+    // const arrayBuffer = new ArrayBuffer(positions.length * Float32Array.BYTES_PER_ELEMENT + colors.length);
+    // const positionBuffer = new Float32Array(arrayBuffer);
+    // const colorBuffer = new Uint8Array(arrayBuffer);
+
+    // let offset = 0;
+    // for (let i = 0; i < positions.length; i += 2) {
+    //     positionBuffer[offset] = positions[i];
+    //     positionBuffer[offset + 1] = positions[i + 1];
+    //     offset += 3;
+    // }
+
+    // offset = 8;
+    // for (let i = 0; i < colors.length; i += 4) {
+    //     colorBuffer[offset] = colors[i];
+    //     colorBuffer[offset + 1] = colors[i + 1];
+    //     colorBuffer[offset + 2] = colors[i + 2];
+    //     colorBuffer[offset + 3] = colors[i + 3];
+    //     offset += 12;
+    // }
 
     const vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
@@ -75,15 +115,33 @@ function main(){
     // gl.DYNAMIC_DRAW：数据会被改变很多。
     // gl.STREAM_DRAW ：数据每次绘制时都会改变
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    // gl.bufferData(gl.ARRAY_BUFFER, arrayBuffer, gl.STATIC_DRAW);
+
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Uint8Array(colors), gl.STATIC_DRAW);
+
+    const indices = [
+        0, 1, 2,
+        2, 1, 3
+    ];
+
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
 
     const fragmentSource = `
     precision mediump float;
+
+    // uniform vec4 u_color;
+
+    varying vec4 v_color;
 
     void main (){
         // 将三角形输出的最终颜色固定为玫红色
         // 这里的四个分量分别代表红（r）、绿（g）、蓝（b）和透明度（alpha）
         // 颜色数值取归一化值。最终绘制的其实就是 [255, 0, 127. 255]
-        gl_FragColor = vec4(1, 0, 0.5, 1);
+        gl_FragColor = v_color;
     }
     `;
 
@@ -114,11 +172,24 @@ function main(){
     // offset：属性在缓冲区中每间隔的偏移值，单位是字节
     // gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
     gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    const colorAttributeLocation = gl.getAttribLocation(program, 'a_color');
+    gl.enableVertexAttribArray(colorAttributeLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(colorAttributeLocation, 4, gl.UNSIGNED_BYTE, true, 0, 0);
     // gl.drawArrays(primitiveType, offset, count);
     // primitiveType：指定图元绘制形式，常见的为绘制点（gl.POINTS），线（gl.LINE_STRIP）和三角面（gl.TRIANGLES）。此处绘制三角形。
     // offset：从哪个点开始绘制
     // count：本次绘制会使用到多少个点。也代表顶点着色器需要运行几次。顶点着色器每次只处理一个顶点。
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    // gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    // const vertexColorLocation = gl.getUniformLocation(program, 'u_color');
+    // gl.uniform4fv(vertexColorLocation, [Math.random(), Math.random(), Math.random(), 1]);
+
+    // gl.drawElements(primitiveType, count, indexType, offset);
+    // 部分参数与 gl.drawArrays 一致。indexType：指定元素数组缓冲区中的值的类型。有 gl.UNSIGNED_BYTE、gl.UNSIGNED_SHORT 以及扩展类型
+    // gl.UNSIGNED_BYTE 最大索引值为 255，gl.UNSIGNED_SHORT 最大索引值为 65535
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 0);
 
 
 }
